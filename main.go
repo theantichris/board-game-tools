af09cf48cfa9c56c7ff3bf3d64259fa682217bd0
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-20 09:27:47 -0400

    changed the sides flag to an integer

--- main.go
+++ main.go
@@ -3,50 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"math/rand"
-	"regexp"
-	"strconv"
 	"time"
 )
 
-const dieFormatMessage = "Format: dx where x is a number. Default d6."
-
 func main() {
 	numberOfDice := flag.Int("n", 1, "The number of dice to roll. Default 1.")
-	diceSides := flag.String("d", "d6", "The type of die to roll. "+dieFormatMessage)
+	diceSides := flag.Int("d", 6, "The number of sides of the dice to roll.")
 	getSum := flag.Bool("s", false, "Get the sum of the dice rolls.")
 	flag.Parse()
 
-	matched, _ := regexp.Match("d\\d+", []byte(*diceSides))
+	rand.Seed(time.Now().UTC().UnixNano())
 
-	if matched {
-		rand.Seed(time.Now().UTC().UnixNano())
+	rolls := rollDice(numberOfDice, diceSides)
+	printRolls(rolls)
 
-		rolls := rollDice(numberOfDice, diceSides)
-		printRolls(rolls)
+	if *getSum {
+		sum := addRolls(rolls)
 
-		if *getSum {
-			sum := addRolls(rolls)
-
-			fmt.Printf("The sum of the rolls is %d.\n", sum)
-		}
-	} else {
-		log.Fatalln("Incorrect format for die. " + dieFormatMessage)
+		fmt.Printf("The sum of the rolls is %d.\n", sum)
 	}
 }
 
-func rollDice(numberOfDice *int, diceSides *string) []int {
-	sides := (*diceSides)[1:]
-	d, err := strconv.Atoi(sides)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+func rollDice(numberOfDice *int, diceSides *int) []int {
 	var rolls []int
 
 	for i := 0; i < *numberOfDice; i++ {
-		rolls = append(rolls, rand.Intn(d)+1)
+		rolls = append(rolls, rand.Intn(*diceSides)+1)
 	}
 
 	return rolls
