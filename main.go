ebff413f7618a5f7c28bdd309709fe792c7f6118
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-20 08:42:09 -0400

    added rolling an x sided die

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+const dieFormatMessage = "Format: dx where x is a number. Default d6."
 
 func main() {
-	fmt.Println("Hello, World")
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	dice := flag.String("d", "d6", "The type of die to roll. "+dieFormatMessage)
+	flag.Parse()
+
+	matched, _ := regexp.Match("d\\d+", []byte(*dice))
+
+	if matched {
+		sides := (*dice)[1:]
+
+		d, err := strconv.Atoi(sides)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		roll := rand.Intn(d) + 1
+		fmt.Printf("You rolled a %d.\n", roll)
+	} else {
+		log.Fatalln("Incorrect format for die. " + dieFormatMessage)
+	}
 }
